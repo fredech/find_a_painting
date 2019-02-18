cf320db35e1f4a464c6e7984f63546f189dface6
Antoine Patouillaud <antoine.patouillaud@gmail.com>
2019-02-18 18:46:53 +0100

    routes et vues

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :paintings, only: [:index, :show, :new, :create, :edit, :update, :destroy ] do
+    resources :bookings, only: [ :create ]
+  end
+  get "/search", to: "paintings#search", as: :search
+  get "/dashboard", to: "users#dashboard", as: :dashboard
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
