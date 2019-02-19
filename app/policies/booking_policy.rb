76ce46a1860df84555c3ca27126b1d819b815d50
Alice Brewer <alicebrewer09@gmail.com>
2019-02-19 14:39:56 +0100

    pundit policies

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -1,7 +1,30 @@
 class BookingPolicy < ApplicationPolicy
+
+  def show?
+    user_is_owner_or_admin?
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    user_is_owner_or_admin?
+  end
+
   class Scope < Scope
     def resolve
-      scope.all
+      if user.admin?
+        scope.all
+      else
+        scope.where(user: user)or(scope.where(user: painting.user))
+      end
     end
+  end
+
+  private
+
+  def user_is_owner_or_admin?
+    record.user == user || user.admin?
   end
 end
