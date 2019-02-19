af66d4f7ce5b92a46febabc368df4edfefa3a049
Alice Brewer <alicebrewer09@gmail.com>
2019-02-19 13:09:31 +0100

    pundit and painting controller

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -1,2 +1,56 @@
 class PaintingsController < ApplicationController
+  before_action :set_painting, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @paintings = policy_scope(Painting)
+    @paintings = Paintings.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @painting = Painting.new
+    authorize @painting
+  end
+
+  def edit
+
+  end
+
+  def create
+    @painting = Painting.new(painting_params)
+    @painting.user = current_user
+    authorize @painting
+    if @painting.save?
+      redirect_to @painting, notice: 'Painting was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @painting.update(painting_params)
+      redirect_to painting_path(@painting_id), notice: 'Painting was successfully updated'
+    else
+      render :edit
+  end
+
+  def destroy
+    @painting.destroy
+    redirect_to paintings_path, notice: 'Painting was successfully destroyed'
+  end
+
+
+  private
+
+  def set_painting
+    @painting = Painting.find(params[:id])
+    authorize @painting
+  end
+
+  def painting_params
+    params.require (:painting).permit(:name, :location, :price, :author, :style, :availabilities, :photo)
+  end
 end
